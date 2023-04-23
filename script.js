c5f9dcb12b30b123973700fab334ece4245e2d5d
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-23 21:53:51 +0530

    Checked?

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.getElementById("submitButton").onclick = function(){
     document.getElementById("myCheck").checked ? console.log('Learning JS') :console.log('Busy');
+
+    let paytmWallet = document.getElementById("paytmWallet");
+    let UPI = document.getElementById("UPI");
+    let debitCard = document.getElementById("debitCard");
+    let creditCard = document.getElementById("creditCard");
+
+    if(paytmWallet.checked){
+        console.log('You Paid via Paytm')
+    }
+    else if(UPI.checked){
+        console.log('You Paid via UPI')
+    }
+    else if(debitCard.checked){
+        console.log('You Paid via Debit Card')
+    }
+    else if(creditCard.checked){
+        console.log('You Paid via Credit Card')
+    }
 }
